c4dad5987ab396a5c8a2ec0ca3ba270c7323ebd1
Oleksandra <oleksandraognista8@gmail.com>
2026-05-30 19:15:43 +0300

    Fix CORS

--- config/settings.py
+++ config/settings.py
@@ -115,6 +115,7 @@ USE_I18N = True
 USE_TZ = True
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ORIGINS = ["https://ohnista-lks07.github.io",]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
